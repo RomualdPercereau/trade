89aa1b58a89e35292ea0132142f260ae1dbd3d98
antoine habert <antoinehabert@yahoo.fr>
2013-05-06 14:41:40 +0200

    merge fonctions de calcul dans la classe Trader

--- projet/Trader.class.php
+++ projet/Trader.class.php
@@ -4,7 +4,6 @@ require "calculs.php";
 
 class Trader
 {
-
 	public $start_capital;
 	public $total_days;
 	public $left_days;
@@ -14,8 +13,40 @@ class Trader
 	public function get_decision()
 	{
 		debug(print_r($this, true));
-		do_calcul($this);
+		$this->do_calcul();
 		return ("wait");
 	}
+
+private function mma($data, $jours)
+{
+	$mma = 0;
+	for ($n = 0; $n < $jours - 1; $n++)
+		$mma += $data[$n];
+	$mma /= $jours;
+	return ($mma);
+}
+
+private function mmp($data, $jours)
+{
+	$mmp = 0;
+	for ($n = 0, $coeff = 0; $n < $jours; $n++)
+	{
+		$mmp += $data[$n] * ($n + 1);
+		$coeff += ($n + 1);
+	}
+	$mmp /= $coeff;
+	return ($mmp);
+}
+
+private function do_calcul()
+{
+	debug("===\n");
+	debug("long terme (mma) :");
+	debug(print_r($this->mma($this->values, $this->days_past), true));
+	debug("\ncourt/moyen terme (mmp) :");
+	debug(print_r($this->mmp($this->values, $this->days_past), true));
+	debug("\n===\n");
+}
+
 
 }
